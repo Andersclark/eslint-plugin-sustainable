dd3c7e6c286af3a93a9e17da4e3d6d75c9e178ae
Anders Clark <anders.clark@gmail.com>
2023-11-28 10:48:19 +0100

    Add test for Accurate Geolocation rule

--- tests/lib/rules/avoid-using-accurate-geolocation.js
+++ tests/lib/rules/avoid-using-accurate-geolocation.js
@@ -1,31 +1,50 @@
 /**
- * @fileoverview High accuracy consumes more power and returns result slower
+ * @fileoverview High accuracy geo location consumes more power and returns result slower
  * @author Anders Clark
  */
-"use strict";
+'use strict'
+const rule = require('../../../lib/rules/avoid-using-accurate-geolocation'),
+  RuleTester = require('eslint').RuleTester
 
-//------------------------------------------------------------------------------
-// Requirements
-//------------------------------------------------------------------------------
+const config = {
+  env: {
+    es6: true,
+  },
+}
 
-const rule = require("../../../lib/rules/avoid-using-accurate-geolocation"),
-  RuleTester = require("eslint").RuleTester;
+const ruleTester = new RuleTester(config)
 
+const errorMessage = {
+  message: 'Avoid high accuracy geolocation',
+  type: 'Property',
+}
 
-//------------------------------------------------------------------------------
-// Tests
-//------------------------------------------------------------------------------
-
-const ruleTester = new RuleTester();
-ruleTester.run("avoid-using-accurate-geolocation", rule, {
+ruleTester.run('avoid-using-accurate-geolocation', rule, {
   valid: [
-    // give me some code that won't trigger a warning
-  ],
-
-  invalid: [
     {
-      code: "enableHighAccuracy: true",
-      errors: [{ message: "Fill me in.", type: "Me too" }],
+      code: `const options = { enableHighAccuracy: false, timeout: 5000, maximumAge: 0 }
+      function success(pos) {
+        console.log(pos.coords);
+      }
+      function error(err) {
+        console.warn(err);
+      }
+      navigator.geolocation.getCurrentPosition(success, error, options);`,
     },
   ],
-});
+  invalid: [
+    {
+      code: `const options = { enableHighAccuracy: true, timeout: 5000, maximumAge: 0 }
+        function success(pos) {
+          console.log(pos.coords);
+        }
+
+        function error(err) {
+          console.warn(err);
+        }
+
+        navigator.geolocation.getCurrentPosition(success, error, options);`,
+      errors: [errorMessage],
+    },
+  ],
+})
